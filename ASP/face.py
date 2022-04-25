c6ff3030822307be202d0135ca4a3786f434a46c
NoySouliya <souliyatmv24@gmail.com>
2022-04-25 12:08:39 +0700

    add face

--- ASP/face.py
+++ ASP/face.py
@@ -1,18 +1,63 @@
 from tkinter import *
 from tkinter import  messagebox
 import  os
-import db
+import pymysql
+import numpy as np
+import cv2
 
-# ຄຳສັ່ງເຊື່ອມຕໍ່
-connection = db.pymysql.connect(host="Localhost", user="root", password="", database="asp_base")
-conn = connection.cursor()
 
+def trainImg():
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    global detector
+    detector = cv2.CascadeClassifier("D:\ASP_Project\ASP\Detect\haarcascade_frontalface_default.xml")
+    try:
+        global faces,Id
+        faces, Id = getImagesAndLabels("ASP/Data")
+    except Exception as e:
+        l='please make "DataSet" folder & put Images'
+        Notification.configure(text=l, bg="SpringGreen3", width=50, font=('times', 18, 'bold'))
+        Notification.place(x=350, y=400)
+
+    recognizer.train(faces, np.array(Id))
+    try:
+        recognizer.save("ASP/Data/trainingData.yml")
+    except Exception as e:
+        q='Please make "Recognizer" folder'
+        Notification.configure(text=q, bg="SpringGreen3", width=50, font=('times', 18, 'bold'))
+        Notification.place(x=350, y=400)
+
+    res = "Model Trained" 
+    Notification.configure(text=res, bg="SpringGreen3", width=50, font=('times', 18, 'bold'))
+    Notification.place(x=250, y=400)
+
+
+
+def getImagesAndLabels(path):
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    faceSamples = []
+    Ids = []
+
+    for imagePath in imagePaths:
+        pilImage = Image.open(imagePath).convert('L')
+        imageNp = np.array(pilImage, 'uint8')
+
+        Id = int(os.path.split(imagePath)[-1].split(".")[1])
+        faces = detector.detectMultiScale(imageNp)
+        for (x, y, w, h) in faces:
+            faceSamples.append(imageNp[y:y + h, x:x + w])
+            Ids.append(Id)
+    return faceSamples, Ids
+
+def insert():
+    window.withdraw()
+    os.system("D:\ASP_Project\ASP\InsertOrUpdateFace.py")
 
 def back():
     l = messagebox.askquestion("BACK","ທ່ານຕ້ອງການຈະກັບໄປໜ້າຫຼັກ ຫຼື ບໍ່?")
     if(l == 'yes'):
         window.withdraw()
         os.system("D:\ASP_Project\ASP\window1.py")
+
 window = Tk()
 window.attributes('-fullscreen', True)
 window.configure(bg = "#ffffff")
@@ -37,7 +82,7 @@ button_1 = Button(
     image=bt1,
     borderwidth=0,
     highlightthickness=0,
-    #command
+    command=insert,
     relief="flat")
 button_1.place(
     x=280.,
@@ -50,7 +95,7 @@ button_2 = Button(
     image=bt2,
     borderwidth=0,
     highlightthickness=0,
-    #command
+    command=trainImg,
     relief="flat"
 )
 button_2.place(
@@ -73,5 +118,10 @@ button_3.place(
     width=246,
     height=90
 )
+
+Notification = Label(window, text="All things are good", bg="Green", fg="white", width=15,
+                height=3, font=('times', 17, 'bold'))
+
+
 window.resizable(False, False)
 window.mainloop()
