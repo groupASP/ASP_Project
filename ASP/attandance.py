797f8890935a2751d4b1897c6771106562ce5960
NoySouliya <souliyatmv24@gmail.com>
2022-04-27 20:39:44 +0700

    add auto att

--- ASP/attandance.py
+++ ASP/attandance.py
@@ -10,36 +10,86 @@ a=tkinter.Tk()
 # a.geometry("1500x900")
 a.attributes("-fullscreen", True)
 
-def scan_face():
+# def scan_face():
+#     import cv2
+#     import pymysql
+
+#     faceDetect = cv2.CascadeClassifier('ASP/Detect/haarcascade_frontalface_default.xml')
+#     cam = cv2.VideoCapture(0)
+#     rec = cv2.face.LBPHFaceRecognizer_create()
+#     rec.read("ASP/Data/trainingImage.yml")
+#     fontface=cv2.FONT_HERSHEY_SIMPLEX
+#     fontScale = 2
+#     fontColor = (255,0,0)
+
+#     def getProfile(Id):
+#         connection = pymysql.connect(host="localhost", user="root", password="", database="asp_base")
+#         conn = connection.cursor()
+#         sql = "SELECT * FROM tb_face where f_Id='"+str(Id)+"';"
+#         conn.execute(sql)
+#         profile=None
+#         for row in conn:
+#             profile=row
+#         conn.close()
+#         return profile
+
+#     while(True):
+#         ret, img = cam.read()
+#         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#         faces = faceDetect.detectMultiScale(gray, 1.3, 5)
+#         for(x, y, w, h) in faces:
+#                 Id, conf = rec.predict(gray[y:y+h, x:x+w])
+#                 if(conf<38):
+#                     print(conf)
+#                     global profile
+#                     profile = getProfile(Id)
+#                     if(profile!=None):
+#                         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+#                         cv2.putText(img, str(profile[0]), (x, y+h+30), fontface, fontScale, fontColor)
+#                         cv2.putText(img, str(profile[1]), (x, y+h+80), fontface, fontScale, fontColor)
+#                 else:
+#                     print("Unknown "+str(conf))
+#                     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+#                     cv2.putText(img, "Unknown", (x, y+h+30), fontface, fontScale, fontColor)
+#         cv2.imshow("Face", img)
+#         key=cv2.waitKey(30) & 0xff == ord('q')
+#         if key:
+#             break
+#     cam.release()
+#     cv2.destroyAllWindows()
+
+def auto_att():
+    import tkinter as tk
     import cv2
     import pymysql
 
     faceDetect = cv2.CascadeClassifier('ASP/Detect/haarcascade_frontalface_default.xml')
     cam = cv2.VideoCapture(0)
     rec = cv2.face.LBPHFaceRecognizer_create()
-    rec.read("ASP/Data/trainingImage.yml")
+    rec.read("ASP\\Data\\trainingData.yml")
     fontface=cv2.FONT_HERSHEY_SIMPLEX
     fontScale = 2
     fontColor = (255,0,0)
 
-    def getProfile(Id):
-        connection = pymysql.connect(host="localhost", user="root", password="", database="asp_base")
-        conn = connection.cursor()
-        sql = "SELECT * FROM tb_face where f_Id='"+str(Id)+"';"
-        conn.execute(sql)
-        profile=None
-        for row in conn:
-            profile=row
-        conn.close()
-        return profile
+    def auto():
+        def getProfile(Id):
+            connection = pymysql.connect(host="localhost", user="root", password="", database="asp_base")
+            conn = connection.cursor()
+            sql = "SELECT * FROM tb_face where f_Id='"+str(Id)+"';"
+            conn.execute(sql)
+            profile=None
+            for row in conn:
+                profile=row
+            conn.close()
+            return profile
 
-    while(True):
-        ret, img = cam.read()
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = faceDetect.detectMultiScale(gray, 1.3, 5)
-        for(x, y, w, h) in faces:
+        while(True):
+            ret, img = cam.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = faceDetect.detectMultiScale(gray, 1.3, 5)
+            for(x, y, w, h) in faces:
                 Id, conf = rec.predict(gray[y:y+h, x:x+w])
-                if(conf<38):
+                if(conf<70):
                     print(conf)
                     global profile
                     profile = getProfile(Id)
@@ -48,16 +98,28 @@ def scan_face():
                         cv2.putText(img, str(profile[0]), (x, y+h+30), fontface, fontScale, fontColor)
                         cv2.putText(img, str(profile[1]), (x, y+h+80), fontface, fontScale, fontColor)
                 else:
-                    print("Unknown "+str(conf))
                     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
                     cv2.putText(img, "Unknown", (x, y+h+30), fontface, fontScale, fontColor)
-        cv2.imshow("Face", img)
-        key=cv2.waitKey(30) & 0xff == ord('q')
-        if key:
-            break
-    cam.release()
-    cv2.destroyAllWindows()
+            cv2.imshow("Face", img)
+            key = cv2.waitKey(1) & 0xFF == ord('q')
+            if key:
+                break
+        try:
+            connection = pymysql.connect(host="localhost", user="root", password="", database="asp_base")
+            conn = connection.cursor()
+        except Exception as e:
+            print(e)
 
+        insert_data =  "INSERT INTO tb_attandance VALUES (0, %s, %s, %s)"
+        VALUES = (str(profile[0]),str(profile[1]), str(profile[2]), str(profile[3]))
+        try:
+            conn.execute(insert_data, VALUES)
+            connection.commit()
+        except Exception as ex:
+            print(ex)
+        cam.release()
+        cv2.destroyAllWindows()
+    auto()
 
 def back():
     l = messagebox.askquestion("BACK","ທ່ານຕ້ອງການຈະກັບໄປໜ້າຫຼັກ ຫຼື ບໍ່?")
@@ -87,7 +149,7 @@ button_1 = Button(
     image=bt1,
     borderwidth=0,
     highlightthickness=0,
-    command=scan_face,
+    command=auto_att,
     relief="flat")
 button_1.place(
     x=500.,
